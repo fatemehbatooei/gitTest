6c442f7b0d0bac5bc2c032fbf0ddcf756805e551
fatemeh <ftm.batooei@gmail.com>
2023-03-03 19:14:01 +0330

    change test by master

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello1");
+        System.out.println("Hello2");
+        System.out.println("Hello3");
+        System.out.println("Hello4");
     }
 }
