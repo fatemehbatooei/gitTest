83670eff1dde616afbdb78bc1410dbab79957053
fatemeh <ftm.batooei@gmail.com>
2023-03-03 19:59:57 +0330

    change all file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void sayHello(){
+        System.out.println("Hello");
+        System.out.println("bye");
+        System.out.println("ridi");
+        System.out.println("naridi");
+
+    }
 }
