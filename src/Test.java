2ae85e3e18af75379b6ff0221edc9ae1fd72550e
fatemehbatooei <88035959+fatemehbatooei@users.noreply.github.com>
2023-03-03 19:49:26 +0330

    change by mamad

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello1");
         System.out.println("Hello122222");
-        System.out.println("Hello133333");
+        System.out.println("salam");
         System.out.println("Hello4");
     }
 }
